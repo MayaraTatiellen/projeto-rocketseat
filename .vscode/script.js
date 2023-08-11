9014aab94498d6e9f6090877311b99bfdb9f60c2
“MayaraTatiellen” <mayaratatiellen@gmail.com>
2023-08-11 19:06:55 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  //pegar a tag img e substituir a imagem
+  // se tiver ligth mode, adicionar uma imagem se estiver sem manter imagem
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/may.png")
+  } else {
+    img.setAttribute("src", "./assets/may2.png")
+  }
+}
